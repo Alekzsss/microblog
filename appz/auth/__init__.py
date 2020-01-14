567d7c61a055bb009c0a5abc1547c667f403f239
Alekzsss <41050907+Alekzsss@users.noreply.github.com>
2020-01-14 21:01:18 +0300

    Update __init__.py

--- appz/auth/__init__.py
+++ appz/auth/__init__.py
@@ -1,10 +1,5 @@
 from flask import Blueprint
-from flask_admin import Admin
 
 bp = Blueprint('auth', __name__)
-admin = Admin()
-
-def create_module(app, **kwargs):
-    admin
 
 from appz.auth import routes
